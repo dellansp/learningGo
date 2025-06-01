7ef6f4794bfd8b08560ebe760a76aac1f08e16b9
dellansp <dellanovitasp11@gmail.com>
2025-06-01 21:03:15 +0700

    BELAJAR VAR CONST POINT

---
+++ variabel.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var lastName string = "Novita"
+	var firstName string = "Della"
+
+	var bilBulat uint8 = 20
+
+	var numberA int = 4
+	var numberB *int = &numberA
+	*numberB = 8
+
+	fmt.Println("halo", firstName, lastName, bilBulat, *numberB, "!\n")
+}
