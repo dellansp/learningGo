d8f9f571e1ce07cb8afd5394d02497f91a70e7ba
dellansp <dellanovitasp11@gmail.com>
2025-06-02 01:20:44 +0700

    learningGo By Della

---
+++ kondisional/switch.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var point = 8
+	switch point {
+	case 8:
+		fmt.Println("perfect")
+	case 7:
+		fmt.Printf("awesome")
+	case 6:
+		fmt.Println("not bad")
+	}
+
+}
---
+++ logika/logika.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var value = true && true // berdasarkan gerbang logika , and ? or? ex and , etc.
+	fmt.Println(value)
+
+}
