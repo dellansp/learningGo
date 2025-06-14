325bcf8fa23521cdecd243f87dcedd4e07ce2269
dellansp <dellanovitasp11@gmail.com>
2025-06-15 04:03:44 +0700

    Learning Golang Della

---
+++ closure/closure.go
@@ -0,0 +1,27 @@
+package main // closure dapat menyimpan function di dlm var , atau func back to func
+
+import (
+	"fmt"
+) // dengan memanggil nama var nya
+
+func main() {
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+		return min, max
+	}
+	var numbers = []int{2, 3, 2, 2, 3, 4}
+	var min, max = getMinMax(numbers)
+	fmt.Println(min)
+	fmt.Println(max)
+
+}
